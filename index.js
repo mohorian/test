deaaede686581a993afbb983c8c5d1f119e4d4ba
mohorian <mohorian1992@gmail.com>
2022-08-21 21:17:27 +0300

    task3

--- index.js
+++ index.js
@@ -1,61 +1,22 @@
-function makeObjectDeepCopy(target) {
-  if (target === null || target === undefined) {
-    return target
-  }
+Array.prototype.myFilter = function myFilter(callback, arr = this) {
+  const output = [];
 
-  if (typeof(target) === 'object') {
-    let cloneTarget = Array.isArray(target) ? [] : {};
-    for (const propety in target) {
-      cloneTarget[propety] = makeObjectDeepCopy(target[propety]);
-    };
-
-    return cloneTarget;
-  };
-
-  return target
-};
-
-function selectFromInterval(arr, number1, number2) {
-  if (!Array.isArray(arr) || typeof(number1) !== 'number' || typeof(number2) !== 'number') {
-    throw new Error();
-  };
-
-  const isValidArray = arr.every((e) => typeof e === 'number');
-
-  if (!isValidArray) {
-    throw new Error();
-  };
-
-  if (number1 > number2) {
-    [number2, number1] = [number1, number2];
-  };
-  return  arr.filter((n) => n >= number1 && n <= number2)
-};
-
-let myIterable = {
-  from: 1,
-  to: 4,
-  [Symbol.iterator]() {
-    let fromNumber = this.from;
-    const toNumber = this.to;
-    const isError = toNumber < fromNumber ||  typeof(fromNumber) !== 'number' || typeof(toNumber) !== 'number';
-    return {
-      next() {
-        if (isError) {
-          throw new Error()
-        };
-
-        if (fromNumber <= toNumber) {
-          return {
-            value: fromNumber++,
-            done: false
-          }
-        } else {
-          return {
-            done: true
-          }
-        };
-      }
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      output.push(arr[i]);
     }
+  };
+
+  return output
+};
+
+function createDebounceFunction(callback, delay) {
+  let timeout;
+
+  return function executedFunction() {
+    const func = () => { callback.apply(this, arguments)};
+
+    clearTimeout(timeout);
+    timeout = setTimeout(func, delay);
   }
 };
---
+++ task2.js
@@ -0,0 +1,64 @@
+function makeObjectDeepCopy(target) {
+  if (target === null || target === undefined) {
+    return target
+  }
+
+  if (typeof(target) === 'object') {
+    const cloneTarget = Array.isArray(target) ? [] : {};
+
+    for (const propety in target) {
+      cloneTarget[propety] = makeObjectDeepCopy(target[propety]);
+    };
+
+    return cloneTarget;
+  };
+
+  return target
+};
+
+function selectFromInterval(arr, number1, number2) {
+  if (!Array.isArray(arr) || typeof(number1) !== 'number' || typeof(number2) !== 'number') {
+    throw new Error();
+  };
+
+  const isValidArray = arr.every((e) => typeof e === 'number');
+
+  if (!isValidArray) {
+    throw new Error();
+  };
+
+  if (number1 > number2) {
+    [number2, number1] = [number1, number2];
+  };
+
+  return  arr.filter((n) => n >= number1 && n <= number2)
+};
+
+let myIterable = {
+  from: 1,
+  to: 4,
+  [Symbol.iterator]() {
+    let fromNumber = this.from;
+    const toNumber = this.to;
+    const isError = toNumber < fromNumber ||  typeof(fromNumber) !== 'number' || typeof(toNumber) !== 'number';
+
+    return {
+      next() {
+        if (isError) {
+          throw new Error()
+        };
+
+        if (fromNumber <= toNumber) {
+          return {
+            value: fromNumber++,
+            done: false
+          }
+        };
+
+        return {
+          done: true
+        }
+      }
+    }
+  }
+};
